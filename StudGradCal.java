3bb2158b9166981f8ca671ddc949b621cbd427d8
GIRILAL <kit29.25bad054@gmail.com>
2026-06-16 10:11:44 +0530

    Add Student Grade Calculator Program

---
+++ StudGradCal.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class StudGradCal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        // Input marks
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter marks for Subject " + i + " (out of 100): ");
+            int marks = sc.nextInt();
+
+            totalMarks += marks;
+        }
+
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / subjects;
+
+        // Grade calculation
+        char grade;
+
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Display results
+        System.out.println("\n----- Result -----");
+        System.out.println("Total Marks = " + totalMarks);
+        System.out.println("Average Percentage = " + averagePercentage + "%");
+        System.out.println("Grade = " + grade);
+
+        sc.close();
+    }
+}
